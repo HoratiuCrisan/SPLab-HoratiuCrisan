de091ad5d59574a7f688323c975afee25c05c116
ReddSnake <92850549+HoratiuCrisan@users.noreply.github.com>
2023-12-23 14:19:55 +0200

    Update ImageLoaderFactory.java

--- SPLab/src/main/java/ro/uvt/info/splab/ImageLoaderFactory.java
+++ SPLab/src/main/java/ro/uvt/info/splab/ImageLoaderFactory.java
@@ -1,16 +1,38 @@
 package ro.uvt.info.splab;
 
-import java.io.IOException;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
 
-public class ImageLoaderFactory {
-    public void createImageLoader(String url) throws IOException {
-        if (url.contains("bmp"))
-            new BMPImageLoader().loadImage(url);
-        else if (url.contains("jpg"))
-            new JPGImageLoader().loadImage(url);
+public class ImageFactory {
+    public void create(String url) throws Exception {
+        if (url.contains(".bmp"))
+            displayImage(new BMPImage().load(url));
+        else if (url.contains(".png"))
+            displayImage(new PNGImage().load(url));
+        else if (url.contains(".jpg"))
+            displayImage(new JPGImage().load(url));
         else
-            throw new IOException("Image type unsupported!");
+            throw new Exception("Unsupported image type !");
     }
 
+    private void displayImage(BufferedImage image) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create an ImageIcon from the Image
+        ImageIcon icon = new ImageIcon(image);
+
+        // Create a JLabel to display the ImageIcon
+        JLabel label = new JLabel(icon);
+
+        // Add the JLabel to the JFrame
+        frame.getContentPane().add(label);
+
+        // Set JFrame properties
+        frame.pack();
+        frame.setLocationRelativeTo(null); // Center the JFrame
+        frame.setVisible(true);
+    }
+}
 
 }
