74a9ee885f6f73fa61bf51d49677ecbd2ca69b9f
ReddSnake <92850549+HoratiuCrisan@users.noreply.github.com>
2023-12-23 14:21:34 +0200

    Create PNGImage.java

---
+++ SPLab/src/main/java/ro/uvt/info/splab/PNGImage.java
@@ -0,0 +1,21 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class PNGImage implements ImageLoader{
+
+    @Override
+    public BufferedImage load(String url) throws IOException {
+        try {
+            // Load image from URL
+            URL imageUrl = new URL(url);
+            BufferedImage image = ImageIO.read(imageUrl);
+
+           return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
