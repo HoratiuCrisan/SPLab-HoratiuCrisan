456e74316af293d51f7f152a1867ca45f68dd9a3
ReddSnake <92850549+HoratiuCrisan@users.noreply.github.com>
2023-12-23 14:18:40 +0200

    Update Main.java

--- SPLab/src/main/java/ro/uvt/info/splab/Main.java
+++ SPLab/src/main/java/ro/uvt/info/splab/Main.java
@@ -3,27 +3,28 @@ package ro.uvt.info.splab;
 public class Main {
     public static void main(String[] args) throws Exception{
         Section cap1 = new Section("Capitolul 1");
-        Paragraph p1 = new Paragraph("Paragraph 1");
-        cap1.add(p1);
-        Paragraph p2 = new Paragraph("Paragraph 2");
-        cap1.add(p2);
-        Paragraph p3 = new Paragraph("Paragraph 3");
-        cap1.add(p3);
-        Paragraph p4 = new Paragraph("Paragraph 4");
-        cap1.add(p4);
-        System.out.println("Printing without Alignment");
-        System.out.println();
-        cap1.print();
-        p1.setAlignStrategy(new AlignCenter());
-        p2.setAlignStrategy(new AlignRight());
-        p3.setAlignStrategy(new AlignLeft());
-
-        System.out.println();
-        System.out.println("Printing with Alignment");
-        System.out.println();
-        cap1.print();
-
-        ImageProxy proxy1 = new ImageProxy("bmp image", "https://people.math.sc.edu/Burkardt/data/bmp/greenland_grid_velo.bmp");
+		Paragraph p1 = new Paragraph("Paragraph 1");
+		cap1.add(p1);
+		Paragraph p2 = new Paragraph("Paragraph 2");
+		cap1.add(p2);
+		Paragraph p3 = new Paragraph("Paragraph 3");
+		cap1.add(p3);
+		Paragraph p4 = new Paragraph("Paragraph 4");
+		cap1.add(p4);
+		System.out.println("Printing without Alignment");
+		System.out.println();
+		cap1.print();
+		p1.setAlignStrategy(new AlignCenter());
+		p2.setAlignStrategy(new AlignRight());
+		p3.setAlignStrategy(new AlignLeft());
+		System.out.println();
+		System.out.println("Printing with Alignment");
+		System.out.println();
+		ImageProxy i = new ImageProxy("https://fileinfo.com/img/ss/xl/bmp_13.png");
+		ImageProxy ii = new ImageProxy("https://people.math.sc.edu/Burkardt/data/bmp/bmp_24.bmp");
+		cap1.add(i);
+		cap1.add(ii);
+		cap1.print();
     }
 
 }
